d020db42f038ccab4adc698e9a46cbd978a5a1f2
type9 <gabespersonal@gmail.com>
2019-08-29 10:25:51 -0700

    Added story

---
+++ madlib.py
@@ -0,0 +1,37 @@
+
+# CREDIT to https://www.madtakes.com/libs/187.html for story
+letterFromCamp_story = [
+                        'Dear ',
+                        'I am having a(n) ',
+                        ' time at camp. The counselor is ',
+                        ' and the food is ',
+                        '.I met ',
+                        ' and we became ',
+                        ' friend. Unfortunately, '
+                        ' is ',
+                        ' and I ',
+                        ' my ',
+                        ' so we could not go ',
+                        ' like everybody else. I need more ',
+                        ' and a ',
+                        ' sharpener, so please ',
+                        ' more when you ',
+                        ' back.'
+                        ]
+ltterFromCamp_words = [
+                        ['RELATIVE', 'WORD NOT SUBMITTED']
+                        ['ADJECTIVE', 'WORD NOT SUBMITTED']
+                        ['ADJECTIVE', 'WORD NOT SUBMITTED']
+                        ['ADJECTIVE', 'WORD NOT SUBMITTED']
+                        ['NAME OF PERSON IN ROOM', 'WORD NOT SUBMITTED']
+                        ['ADJECTIVE', 'WORD NOT SUBMITTED']
+                        ['ADJECTIVE', 'WORD NOT SUBMITTED']
+                        ['VERB ENDING IN "ED"', 'WORD NOT SUBMITTED']
+                        ['BODY PART', 'WORD NOT SUBMITTED']
+                        ['VERB ENDING IN "ING"', 'WORD NOT SUBMITTED']
+                        ['NOUN (PLURAL)', 'WORD NOT SUBMITTED']
+                        ['NOUN', 'WORD NOT SUBMITTED']
+                        ['ADVERB', 'WORD NOT SUBMITTED']
+                        ['VERB', 'WORD NOT SUBMITTED']
+                        ['VERB', 'WORD NOT SUBMITTED']
+]
